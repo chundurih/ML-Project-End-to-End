c3a54902d81b03561b564c5a866cd9ce279adb5b
chundurih <chundurihvr@gmail.com>
2023-06-17 18:54:58 -0500

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,31 @@
+from os import name
+from setuptools import find_packages, setup
+from typing import List
+
+iden='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will retur the list of requirements
+    '''
+    requirements=[]
+    
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("/n","") for req in requirements ]
+
+        if iden in requirements:
+            requirements.remove(iden)
+
+    return(requirements)
+
+
+
+setup(
+name='endtoendmlproject',
+version='0.0.1',
+author='Harsha Chunduri',
+author_email='hvr.chunduri@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
