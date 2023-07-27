b615f7fc323d503d2efb5b49a228502fb727d488
chundurih <chundurihvr@gmail.com>
2023-07-26 23:39:31 -0500

    Transformation p1

---
+++ src/Components/data_transformation.py
@@ -0,0 +1,63 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.join('artifacts',"preprocessor.pkl")
+
+class datatransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+    
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns=["writing_Score","reading_score"]
+            categorical_columns=[
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course"
+            ]
+
+            num_pipeline=Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(Stategy="median")),
+                    ("scaler",StandardScaler())
+                      ])
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(Stategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder())
+                    ("scaler",StandardScaler())
+                      ])
+
+            logging.info("numerical columns scaling completed")
+            logging.info("categorical columns encoding completed")
+
+            preprocessor=ColumnTransformer(
+                ("num_pipeline",num_pipeline,numerical_columns)
+                ("cat_pipeline",cat_pipeline,categorical_columns)
+
+                                          )
+
+            return preprocessor
+
+
+            
+
+
+            
+        except:
+            pass
+
